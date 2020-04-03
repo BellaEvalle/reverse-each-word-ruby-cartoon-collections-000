f58d402d74c83b2a6e95813a07b55b3d9e3c51b7
Bella Evalle <bellaevalle@yahoo.com>
2020-04-03 21:27:41 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word (sentence)
+  array = sentence.split(" ")
+array.each do |word|
+  word.reverse!
+end
+array.join(" ")
+end
+
+def reverse_each_word (sentence)
+  array = sentence.split(" ")
+array.collect do |word|
+  word.reverse
+end.join(" ")
+end
